f2653acd4450141994695c72231b37a3cb2377c6
Maciej Budzyński <maciejb@virtualdesign.pl>
2020-10-11 16:04:39 +0200

    new api xml, add array to querySelectorAll and map results

--- src/pages/News/News.js
+++ src/pages/News/News.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Container, ArticleWrapper, Title, Description, Image} from "./News.css";
 import {Button} from "../../components";
 import IMG from "../../assets/img/finance.jpg";
 import Carousel from "react-elastic-carousel";
+import {ResultsDemographics} from "styled-icons/foundation";
 
 const News = () => {
+  const [result, setResult] = useState([]);
+
+  const API = "https://rss.app/feeds/oNwptth2JLtqxDVx.xml";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const buffer = await fetch(API);
+      const content = await buffer.text();
+
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(content, "text/xml");
+      const items = Array.from(doc.querySelectorAll("item"));
+      console.log(items.length);
+      setResult(
+        items.map((item) => {
+          return {
+            title: item.children[0].textContent,
+          };
+        })
+      );
+      // console.log(items);
+    };
+    fetchData();
+  }, [API]);
+
   return (
     <Container>
       <Carousel>
-        <ArticleWrapper>
+        {result.map(({title}) => (
+          <Title>{title}</Title>
+        ))}
+        {/* <ArticleWrapper>
           <Title>5 Financial Things to Consider Before Quitting</Title>
           <Image src={IMG} alt="finance image" />
           <Description>
@@ -18,13 +47,13 @@ const News = () => {
             up.
           </Description>
           <Button readMore>Read more</Button>
-        </ArticleWrapper>
-        <ArticleWrapper>
+        </ArticleWrapper> */}
+        {/* <ArticleWrapper>
           <Title>ziomeczku elo</Title>
           <Image src={IMG} alt="finance image" />
           <Description>lorem lorem ipsum ipsmu finfnaoano kod...</Description>
           <Button readMore>Read more</Button>
-        </ArticleWrapper>
+        </ArticleWrapper> */}
       </Carousel>
     </Container>
   );
