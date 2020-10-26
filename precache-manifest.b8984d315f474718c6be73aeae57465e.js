780daee9a1cc867a02381ab07a13ec50f1df33f4
Maciej Budzyński <maciejb@virtualdesign.pl>
2020-10-26 16:50:14 +0100

    Updates

---
+++ precache-manifest.b8984d315f474718c6be73aeae57465e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e18424b88556fb2fbc08b8c242ae4637",
+    "url": "/exchanger-vol2/index.html"
+  },
+  {
+    "revision": "48ebab3ab33d6ed3236e",
+    "url": "/exchanger-vol2/static/js/2.36fbe3af.chunk.js"
+  },
+  {
+    "revision": "06d840de4768eb118188e6bf94536dc9",
+    "url": "/exchanger-vol2/static/js/2.36fbe3af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a5d708bb580ae559723",
+    "url": "/exchanger-vol2/static/js/main.71c6ab3c.chunk.js"
+  },
+  {
+    "revision": "b7560ff692256a098dcd",
+    "url": "/exchanger-vol2/static/js/runtime-main.4ac0b781.js"
+  },
+  {
+    "revision": "e1996192b98a516646ff9a8c0c0ca90c",
+    "url": "/exchanger-vol2/static/media/Oswald-Regular.e1996192.ttf"
+  },
+  {
+    "revision": "63fa626fecc14d7d9101fa6ae7711f09",
+    "url": "/exchanger-vol2/static/media/dollar.63fa626f.svg"
+  },
+  {
+    "revision": "b104585010b0a558fe969444fec6538f",
+    "url": "/exchanger-vol2/static/media/euro.b1045850.svg"
+  },
+  {
+    "revision": "cd9cbeb5cd9534feb250e0b9bc0870c5",
+    "url": "/exchanger-vol2/static/media/pound.cd9cbeb5.svg"
+  },
+  {
+    "revision": "985488b1b02c2ad17d5a12169bb6bc78",
+    "url": "/exchanger-vol2/static/media/shekel.985488b1.svg"
+  },
+  {
+    "revision": "6971094e911033533dbf2f247c90f14f",
+    "url": "/exchanger-vol2/static/media/won.6971094e.svg"
+  },
+  {
+    "revision": "a280f2a8d4abddb50b07c1774de8c160",
+    "url": "/exchanger-vol2/static/media/yen.a280f2a8.svg"
+  }
+]);
